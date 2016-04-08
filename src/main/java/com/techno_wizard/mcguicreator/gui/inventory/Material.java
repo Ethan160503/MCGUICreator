0568f6a9d9201d45a058ae5fa69d82667661d30d
ZombieStriker <dannyboyn@yahoo.com>
2016-04-07 21:09:11 -0400

    Added blocks to the Material enum and imported all block icons from the MCWiki.

--- src/main/java/com/techno_wizard/mcguicreator/gui/inventory/Material.java
+++ src/main/java/com/techno_wizard/mcguicreator/gui/inventory/Material.java
@@ -7,8 +7,10 @@ import java.awt.image.BufferedImage;
  * Created by Ethan on 4/3/2016.
  */
 public enum Material {
+    //Items
     AIR("Null","Air"),
     ACACIA_BOAT("/itemimg/acacia_boat.png","Acaial Boat"),
+    ACACIA_STEPS("blockimg/acacia_steps.png","Acacia Steps","ACACIA_STEPS",0),
     APPLE("/itemimg/apple.png","Apple"),
     APPLE_GOLDEN("/itemimg/apple_golden.png","Golden Apple"),
     ARROW("/itemimg/arrow.png","Arrow"),
@@ -54,6 +56,13 @@ public enum Material {
     COAL("/itemimg/bow.png","Bow"),
     POISONOUS_POTATO("/itemimg/potato_poisonous.png","Poisonous Potato"),
     POTATO("/itemimg/potato.png","Potato"),
+    STONE_STONE("blockimg/stone.png","Stone","STONE",0),
+    STONE_GRANITE("blockimg/stone.png","Stone","STONE",1),
+    STONE_GRANITE_SMOOTH("blockimg/stone.png","Stone","STONE",2),
+    STONE_DIORITE("blockimg/stone.png","Stone","STONE",3),
+    STONE_DIORITE_SMOOTH("blockimg/stone.png","Stone","STONE",4),
+    STONE_ANDERSITE("blockimg/stone.png","Stone","STONE",5),
+    STONE_ANDERSITE_SMOOTH("blockimg/stone.png","Stone","STONE",6),
     WOOD_SWORD("/itemimg/wood_sword.png","Wood sword"),
 
     //Dyes
@@ -72,7 +81,113 @@ public enum Material {
     DYE_BROWN("/itemimg/dye_powder_brown.png","Brown Dye","INK_SACK",12),
     DYE_GREEN("/itemimg/dye_powder_green.png","Green Dye","INK_SACK",13),
     DYE_RED("/itemimg/dye_powder_red.png","Red Dye","INK_SACK",14),
-    DYE_BLACK("/itemimg/dye_powder_black.png","Black Dye","INK_SACK",15);
+    DYE_BLACK("/itemimg/dye_powder_black.png","Black Dye","INK_SACK",15),
+
+    //TODO: Put the following blocks in alphabetical order back in the section above
+    ANVIL("blockimg/stone.png","Anvil"),
+    BEACON("blockimg/beacon.png","Beacon"),
+    BEDROCK("blockimg/bedrock.png","Bedrock"),
+    BIRCH_WOOD_STEPS("blockimg/birch_wood_steps.png","Birch Stairs"),
+    BOOKSHELF("blockimg/bookshelf.png","Booshelf"),
+    BRICK_BLOCK("blockimg/brick_block.png","Bricks"),//may be called BRICKS
+    BRICK_STEPS("blockimg/brick_steps.png","Brick stairs"),
+    CACTUS("blockimg/cactus.png","Cactus"),
+    CHEST("blockimg/chest.png","Chest"),
+    CLAY_BLOCK("blockimg/clay_block.png","Clay Block"),//may be called CLAY
+    COAL_BLOCK("blockimg/coal_block.png","Coal Block"),
+    COBBLESTONE_FENCE("blockimg/cobble_stone_fence.png","Cobblestone fence"),//may be called something different
+    COBBLESTONE("blockimg/cobblestone.png","Cobblestone"),
+    CRAFTING_TABLE("blockimg/crafting_table.png","Crafting Table"),//I could not find this in the Material Enum for bukkit
+    DARK_OAK_STEPS("blockimg/stone.png","Dark Oak Stairs"),
+    DAYLIGHT_DETECTOR("blockimg/stone.png","Daylight Detector"),//may be called something different
+    DIAMOND_BLOCK("blockimg/stone.png","Diamond Block"),
+    DIAMOND_ORE("blockimg/stone.png","Diamond Ore"),
+    DIRT("blockimg/stone.png","Dirt"),
+    DISPENCER("blockimg/stone.png","Dispencer"),
+    DRAGON_EGG("blockimg/stone.png","Dragon Egg"),
+    DROPPER("blockimg/stone.png","Dropper"),
+    EMERALD_BLOCK("blockimg/stone.png","Emerald Block"),
+    EMERALD_ORE("blockimg/stone.png","Emerald Ore"),
+    ENCHANTMENT_TABLE("blockimg/stone.png","Enchantment Table"),
+    END_PORTAL("blockimg/stone.png","End Portal"),
+    END_PORTAL_FRAME("blockimg/stone.png","End Portal Frame"),//may be called something different
+    END_STONE("blockimg/stone.png","End Stone"),
+    WOOD_FENCE_GATE("blockimg/stone.png","Fence Gate"),//may be called something different
+    FIRE("blockimg/stone.png","Fire"),
+    FURNACE("blockimg/stone.png","Furnace"),
+    GLASS("blockimg/stone.png","Glass"),
+    GLASS_PANE("blockimg/stone.png","Glass Pane"),
+    GLOWSTONE("blockimg/stone.png","GlowStone"),
+    GOLD_BLOCK("blockimg/stone.png","Gold Block"),
+    GOLD_ORE("blockimg/stone.png","Gold Ore"),
+    GOLD_PRESSURE_PLATE("blockimg/stone.png","Gold/Light Weight Pressure Plat"),
+    GRASS("blockimg/stone.png","Grass"),
+    GRAVEL("blockimg/stone.png","Gravel"),
+    HARDENED_CLAY("blockimg/stone.png","Hardened_Clay"),
+    HAY_BLOCK("blockimg/stone.png","Hay_Block"),
+    ICE("blockimg/stone.png","Ice"),
+    IRON_BARS("blockimg/stone.png","Iron Bars"),
+    IRON_BLOCK("blockimg/stone.png","Iron Block"),
+    IRON_ORE("blockimg/stone.png","Iron) Ore"),
+    IRON_PRESSURE_PLATE("blockimg/stone.png","Iron/Heavy Pressure Plate"),
+    JUKEBOX("blockimg/stone.png","JukeBox"),
+    JUNGLE_WOOD_STEPS("blockimg/stone.png","Jungle Wood Stairs"),//may be called something else
+    LAPIS_BLOCK("blockimg/stone.png","Lapis Block"),
+    LAPIS_ORE("blockimg/stone.png","Lapis Ore"),//may be called something else
+    LEAVES("blockimg/stone.png","Leaves"),
+    LOG("blockimg/stone.png","Log"),
+    MELON_BLOCK("blockimg/stone.png","Melon Block"),
+    MOB_SPAWNER("blockimg/stone.png","Mob Spawner"),//may be called something else
+    MYCELIUM("blockimg/stone.png","Mycelium"),
+    NETHER_BRICK("blockimg/stone.png","Nether Brick"),
+    NETHER_BRICK_FENCE("blockimg/stone.png","Nether Brick Fence"),//may be called something else
+    NETHER_BRICK_STEPS("blockimg/stone.png","Nether Brick Steps"),
+    NETHERRACK("blockimg/stone.png","Netherrack"),
+    NOTE_BLOCK("blockimg/stone.png","Note Block"),
+    OBSIDIAN("blockimg/stone.png","Obsidian"),
+    PACKED_ICE("blockimg/stone.png","Packed Ice"),
+    PISTON("blockimg/stone.png","Piston"),
+    PRISMARINE("blockimg/stone.png","Prismarine"),
+    PUMPKIN("blockimg/stone.png","Pumpkin"),
+    PUMPKIN_STEM("blockimg/stone.png","Pumpkin Stem"),//may be called something differrnt
+    PURPUR_BLOCK("blockimg/stone.png","Purpur block"),
+    QUARTZ_BLOCK("blockimg/stone.png","Quartz Block"),
+    QUARTZ_ORE("blockimg/stone.png","Quartz ore"),
+    QUARTZ_STEPS("blockimg/stone.png","Quartz Steps"),
+    RESTONE_BLOCK("blockimg/stone.png","Redstone Block"),
+    REDSTONE_LAMP_OFF("blockimg/stone.png","Redstone Lamp Off"),
+    REDSTONE_LAMP_ON("blockimg/stone.png","Redstone Lamp on"),
+    REDSTONE_ORE("blockimg/stone.png","Redstone Ore"),
+    SAND("blockimg/stone.png","Sand"),
+    SANDSTONE("blockimg/stone.png","Sand Stone"),
+    SANDSTONE_STEPS("blockimg/stone.png","Sandstone Stairs"),
+    SAPLING("blockimg/stone.png","Sapling"),
+    SEA_LANTERN("blockimg/stone.png","Sea Lantern"),
+    SKULL("blockimg/stone.png","Skull"),//keeping this called "skull" even though it's a block.
+    SLIME_BLOCK("blockimg/stone.png","Slime Block"),
+    SNOW_BLOCK("blockimg/stone.png","Snow Block"),//may be something different
+    SNOW_LAYER("blockimg/stone.png","Snow Layer"),
+    SPONGE("blockimg/stone.png","Sponge"),
+    SPRUCE_WOOD_STEPS("blockimg/stone.png","Spruce Wood Stairs"),//may be something diffeent
+    STAINED_CLAY("blockimg/stone.png","Stained Clay"),
+    STAINED_GLASS("blockimg/stone.png","Stained Glass"),
+    STAINED_GLASS_PANE("blockimg/stone.png","Stained Glass Pane"),
+    STICKY_PISTON("blockimg/stone.png","Sticky Piston"),
+    STONE_BRICK("blockimg/stone.png","Stone Brick"),//may be called something different
+    STONE_BRICK_STEPS("blockimg/stone.png","Stone Brick Stairs"),
+    STONE_PRESSURE_PLATE("blockimg/stone.png","Stone Pressure Plate"),
+    STONE_SLAB("blockimg/stone.png","Stone Slab"),
+    TNT("blockimg/stone.png","TNT"),
+    TRAPPED_CHEST("blockimg/stone.png","Trapped Chest"),
+    VINE("blockimg/stone.png","Vine"),
+    WATERLILLY("blockimg/stone.png","Water Lilly"),//may be called something else
+    WOOD("blockimg/stone.png","Wood"),
+    WOOD_FENCE("blockimg/stone.png","Wood Fence"),
+    WOOD_SLAB("blockimg/stone.png","Wood Slab"),
+    WOOD_STEPS("blockimg/stone.png","Wood Stairs"),
+    WOOL_CARPET_WHITE("blockimg/stone.png","Wool Carpet White"),
+    WOOL_WHITE("blockimg/stone.png","Wool White")
+    ;
 
     private ImageIcon image;
     private String name;
@@ -106,6 +221,7 @@ public enum Material {
     public String getMaterialEnumName(){
         return "Material."+materialName;
     }
+    public String getDisplayName(){ return name; }
 
     /**
      * This returns the name of each materials. Use this to turn the material into a string.
@@ -121,7 +237,7 @@ public enum Material {
 
     public Material getMaterialByName(String name){
         for(Material m: Material.values()){
-            if(m.getName().equals(name))
+            if(m.getDisplayName().equals(name))
                 return m;
         }
         return null;
