933c40a4c3455982dea462dea124023ade84d498
ethan <ethanzeigler@gmail.com>
2016-05-07 08:41:20 -0400

    Partially working constructors, but having issue pointing to the template file at runtime

--- src/main/java/com/techno_wizard/mcguicreator/codecreator/CodeGenerator.java
+++ src/main/java/com/techno_wizard/mcguicreator/codecreator/CodeGenerator.java
@@ -1,7 +1,11 @@
 package com.techno_wizard.mcguicreator.codecreator;
 
 import com.techno_wizard.mcguicreator.gui.InventoryTableModel;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +13,25 @@ import java.util.List;
  * A generator that takes an inventory and outputs code to make it in MC
  */
 public class CodeGenerator {
+
+    private static CodeGenerator instance;
+    private JSONObject codeTemplates;
+
+    public static CodeGenerator getInstance() {
+        if (instance == null)
+            instance = new CodeGenerator();
+
+        return instance;
+    }
+
+    private CodeGenerator() {
+        try {
+            codeTemplates = (JSONObject) JSONValue.parse(new FileReader("/home/ethan/IdeaProjects/MCGUICreator/target/classes/code_templates.json"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Code templates file failed to load");
+        }
+    }
 
     /**
      * A wrapper for an array list ot also give indent formatting
@@ -61,8 +84,8 @@ public class CodeGenerator {
      * Writes the java code that constructs the inventory table model in bukkit
      * @return
      */
-    public static String writeRepresentingJava(InventoryTableModel invModel) {
-
+    public String writeRepresentingJava(InventoryTableModel invModel) {
+        return null;
     }
 
     /**
@@ -74,10 +97,50 @@ public class CodeGenerator {
      * @return java of data member constructor
      *
      */
-    private static String getJavaMemeberConstructor(Object wrapperValue) {
-        if(wrapperValue.getClass().isAssignableFrom(Integer.class)) {
+    private String getJavaMemeberConstructor(Object wrapperValue, String varName) {
+        JSONObject constructors = (JSONObject) codeTemplates.get("java constructors");
+        String template;
+        if(wrapperValue instanceof Integer) {
             Integer integer = (Integer) wrapperValue;
-            //todo add java constructors to code generation template file
+            template = (String) constructors.get("int");
+            template.replace("$name$", varName);
+            template.replace("$val$", integer.toString());
+        } else if (wrapperValue instanceof Double) {
+            Double dub = (Double) wrapperValue;
+            template = (String) constructors.get("double");
+            template.replace("$name$", varName);
+            template.replace("$val$", dub.toString());
+        } else if (wrapperValue instanceof Short) {
+            Short shrt = (Short) wrapperValue;
+            template = (String) constructors.get("short");
+            template.replace("$name$", varName);
+            template.replace("$val$", shrt.toString());
+        } else if (wrapperValue instanceof Character) {
+            Character ctr = (Character) wrapperValue;
+            template = (String) constructors.get("char");
+            template.replace("$name$", varName);
+            template.replace("$val$", ctr.toString());
+        } else if (wrapperValue instanceof Float) {
+            Float flot = (Float) wrapperValue;
+            template = (String) constructors.get("float");
+            template.replace("$name$", varName);
+            template.replace("$val$", flot.toString());
+        } else if (wrapperValue instanceof String) {
+            String string = (String) wrapperValue;
+            template = (String) constructors.get("String");
+            template.replace("$name$", varName);
+            template.replace("$val$", string);
+        } else {
+            System.out.println("Error: could not determine java class type");
+            template = "Error";
         }
+        return template;
+    }
+
+    public static void main(String[] args) {
+        //todo test for file reading
+        CodeGenerator gen = CodeGenerator.getInstance();
+        System.out.println("outut: " + gen.getJavaMemeberConstructor("uhh", "var"));
+
     }
 }
