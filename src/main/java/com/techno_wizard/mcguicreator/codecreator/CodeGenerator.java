2e26f74db54ef10cf58e28c70403366f6d389fb0
ethan <ethanzeigler@gmail.com>
2016-05-18 19:10:30 -0400

    Fixed code indentation, some other stuff. Generator is filled with errors out the wazoo, but I had to stop for the day

--- src/main/java/com/techno_wizard/mcguicreator/codecreator/CodeGenerator.java
+++ src/main/java/com/techno_wizard/mcguicreator/codecreator/CodeGenerator.java
@@ -17,7 +17,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * A generator that takes an inventory and outputs code to make it in MC
+ * A generator that takes an inventoryStringMap and outputs code to make it in MC
  */
 public class CodeGenerator {
 
@@ -25,13 +25,13 @@ public class CodeGenerator {
     private MainMenu mainMenu;
     private JSONObject codeTemplates;
 
-    private JSONObject comments;
-    private JSONObject inventory;
-    private JSONObject javaConstructors;
-    private JSONObject itemStack;
-    private JSONObject lore;
-    private JSONObject enchantment;
-    private JSONObject extra;
+    private JSONObject commentsStringMap;
+    private JSONObject inventoryStringMap;
+    private JSONObject javaConstructorsStringMap;
+    private JSONObject itemStackStringMap;
+    private JSONObject loreStringMap;
+    private JSONObject enchantmentStringMap;
+    private JSONObject extraStringMap;
 
     private Pattern openBracketPattern;
     private Pattern closeBracketPattern;
@@ -55,13 +55,13 @@ public class CodeGenerator {
 
         this.mainMenu = MCGUICreator.getMainMenu();
 
-        comments = (JSONObject) codeTemplates.get("comments");
-        inventory = (JSONObject) codeTemplates.get("inventory");
-        javaConstructors = (JSONObject) codeTemplates.get("java constructors");
-        itemStack = (JSONObject) codeTemplates.get("itemstack");
-        lore = (JSONObject) codeTemplates.get("lore");
-        enchantment = (JSONObject) codeTemplates.get("enchantment");
-        extra = (JSONObject) codeTemplates.get("extra");
+        commentsStringMap = (JSONObject) codeTemplates.get("comments");
+        inventoryStringMap = (JSONObject) codeTemplates.get("inventory");
+        javaConstructorsStringMap = (JSONObject) codeTemplates.get("java constructors");
+        itemStackStringMap = (JSONObject) codeTemplates.get("itemstack");
+        loreStringMap = (JSONObject) codeTemplates.get("lore");
+        enchantmentStringMap = (JSONObject) codeTemplates.get("enchantment");
+        extraStringMap = (JSONObject) codeTemplates.get("extra");
 
         openBracketPattern = Pattern.compile("[{]");
         closeBracketPattern = Pattern.compile("[}]");
@@ -83,17 +83,13 @@ public class CodeGenerator {
         }
 
         public String getFormattedOutput() {
-            return format();
-        }
-
-        private String format() {
             StringBuilder sb = new StringBuilder();
             int runningIndentCount = 0;
             // for each line
 
             Iterator<String> iterator = code.iterator();
             while (iterator.hasNext()) {
-                String next = iterator.next();;
+                String next = iterator.next();
 
                 // find counts of open and closes.
                 Matcher openMatcher = openBracketPattern.matcher(next);
@@ -113,12 +109,12 @@ public class CodeGenerator {
                     for(int i = 0; runningIndentCount - lineIndentMovement > i; i++) {
                         totalIndent += INDENT;
                     }
-                    sb.append(totalIndent + next);
-                } else if (lineIndentMovement > 0) {
+                    sb.append(totalIndent + next + "\n");
+                } else if (lineIndentMovement >= 0) {
                     for (int i = 0; runningIndentCount > i; i++) {
                         totalIndent += INDENT;
                     }
-                    sb.append(totalIndent + next);
+                    sb.append(totalIndent + next + "\n");
                 }
                 runningIndentCount += lineIndentMovement;
                 if (runningIndentCount < 0)
@@ -126,116 +122,127 @@ public class CodeGenerator {
             }
             return sb.toString();
         }
+    }
+    /**
+     * Writes the java code that constructs the inventoryStringMap table model in bukkit
+     * @return
+     */
+    public String writeRepresentingJava(InventoryTableModel invModel){
+        CodeBuffer buffer = new CodeBuffer();
 
-        /**
-         * Writes the java code that constructs the inventory table model in bukkit
-         * @return
-         */
-        public String writeRepresentingJava(InventoryTableModel invModel){
-            CodeBuffer buffer = new CodeBuffer();
+        buffer.addLine((String) commentsStringMap.get("init comment"));
+        buffer.addLine(((String) extraStringMap.get("class constructor"))
+                .replace("$count$",mainMenu.getInvManager().getInventoryTableModel().getRowCount()*9+"")
+                .replace("$name$",mainMenu.getInvManager().getInventoryTableModel().getInventoryName())
+                .replace("$inv$","inventoryInstance")
+                .replace("$classname$",mainMenu.getInvManager().getInventoryTableModel().getInventoryName().replace(" ","")+"Inventory")
+        );
 
-            buffer.addLine((String) comments.get("init comment"));
-            buffer.addLine(((String) extra.get("class constructor"))
-                    .replace("$count$",mainMenu.getInvManager().getInventoryTableModel().getRowCount()*9+"")
-                    .replace("$name$",mainMenu.getInvManager().getInventoryTableModel().getInventoryName())
-                    .replace("$inv$","inventoryInstance")
-                    .replace("$classname$",mainMenu.getInvManager().getInventoryTableModel().getInventoryName().replace(" ","")+"Inventory")
-            );
-
-            buffer.addLine((String) extra.get("init inv"));
-            for(int slot = 0; slot < mainMenu.getInvManager().getInventoryTableModel().getRowCount()*9;slot++){
-                if(mainMenu.getInvManager().getInventoryTableModel().getItemStackAt(slot%9,slot/9).getMaterial() != Material.AIR){
-                    addItemstackToInv(buffer,slot);
-                }
+        buffer.addLine((String) extraStringMap.get("init inv"));
+        for(int slot = 0; slot < mainMenu.getInvManager().getInventoryTableModel().getRowCount()*9;slot++){
+            ItemStack toWrite = mainMenu.getInvManager().getInventoryTableModel().getItemStackAt(slot%9,slot/9);
+            if(toWrite.getMaterial() != Material.AIR){
+                addItemstackToInv(toWrite, buffer,slot);
             }
-
-            return buffer.getFormattedOutput();
         }
 
-        /**
-         * Gets java data members' constructors. The following are supported<br>
-         * int, double, short, float, String, char
-         *
-         * @param wrapperValue the value to get its constructor of.
-         *
-         * @return java of data member constructor
-         *
-         */
-        private String getJavaMemeberConstructor(Object wrapperValue, String varName) {
-            JSONObject constructors = (JSONObject) codeTemplates.get("java constructors");
-            String template;
-            if(wrapperValue instanceof Integer) {
-                Integer integer = (Integer) wrapperValue;
-                template = (String) constructors.get("int");
-                template.replace("$name$", varName);
-                template.replace("$val$", integer.toString());
-            } else if (wrapperValue instanceof Double) {
-                Double dub = (Double) wrapperValue;
-                template = (String) constructors.get("double");
-                template.replace("$name$", varName);
-                template.replace("$val$", dub.toString());
-            } else if (wrapperValue instanceof Short) {
-                Short shrt = (Short) wrapperValue;
-                template = (String) constructors.get("short");
-                template.replace("$name$", varName);
-                template.replace("$val$", shrt.toString());
-            } else if (wrapperValue instanceof Character) {
-                Character ctr = (Character) wrapperValue;
-                template = (String) constructors.get("char");
-                template.replace("$name$", varName);
-                template.replace("$val$", ctr.toString());
-            } else if (wrapperValue instanceof Float) {
-                Float flot = (Float) wrapperValue;
-                template = (String) constructors.get("float");
-                template.replace("$name$", varName);
-                template.replace("$val$", flot.toString());
-            } else if (wrapperValue instanceof String) {
-                String string = (String) wrapperValue;
-                template = (String) constructors.get("String");
-                template.replace("$name$", varName);
-                template.replace("$val$", string);
-            } else {
-                System.out.println("Error: could not determine java class type");
-                template = "Error";
-            }
-            return template;
+        return buffer.getFormattedOutput();
+    }
+
+    /**
+     * Gets java data members' constructors. The following are supported<br>
+     * int, double, short, float, String, char
+     *
+     * @param wrapperValue the value to get its constructor of.
+     *
+     * @return java of data member constructor
+     *
+     */
+    private String getJavaMemeberConstructor(Object wrapperValue, String varName) {
+        JSONObject constructors = (JSONObject) codeTemplates.get("java constructors");
+        String template;
+        if(wrapperValue instanceof Integer) {
+            Integer integer = (Integer) wrapperValue;
+            template = (String) constructors.get("int");
+            template.replace("$name$", varName);
+            template.replace("$val$", integer.toString());
+        } else if (wrapperValue instanceof Double) {
+            Double dub = (Double) wrapperValue;
+            template = (String) constructors.get("double");
+            template.replace("$name$", varName);
+            template.replace("$val$", dub.toString());
+        } else if (wrapperValue instanceof Short) {
+            Short shrt = (Short) wrapperValue;
+            template = (String) constructors.get("short");
+            template.replace("$name$", varName);
+            template.replace("$val$", shrt.toString());
+        } else if (wrapperValue instanceof Character) {
+            Character ctr = (Character) wrapperValue;
+            template = (String) constructors.get("char");
+            template.replace("$name$", varName);
+            template.replace("$val$", ctr.toString());
+        } else if (wrapperValue instanceof Float) {
+            Float flot = (Float) wrapperValue;
+            template = (String) constructors.get("float");
+            template.replace("$name$", varName);
+            template.replace("$val$", flot.toString());
+        } else if (wrapperValue instanceof String) {
+            String string = (String) wrapperValue;
+            template = (String) constructors.get("String");
+            template.replace("$name$", varName);
+            template.replace("$val$", string);
+        } else {
+            System.out.println("Error: could not determine java class type");
+            template = "Error";
         }
+        return template;
+    }
 
 
-        private void addItemstackToInv(ItemStack itemstack, CodeBuffer buffer){
+    private void addItemstackToInv(ItemStack itemStack, CodeBuffer buffer, int slot) {
 
-            String shouldHaveData = itemStack..getDurability() == 0 ? "create no data" : "create with data";
-            buffer.addLine(((String)itemStack.get(shouldHaveData))
-                    .replace("$material$",itemStack1.getMaterial().getMaterialEnumName())
-                    .replace("$count$",itemStack1.getAmount()+"")
-                    .replace("$data$",itemStack1.getMaterial().getDurability()+"")
-                    .replace("$stack$","itemStack"+slot)
+        String shouldHaveData = itemStack.getMaterial().getDurability() == 0 ? "create no data" : "create with data";
+        buffer.addLine(((String) itemStackStringMap.get(shouldHaveData))
+                .replace("$material$", itemStack.getMaterial().getMaterialEnumName())
+                .replace("$count$", itemStack.getAmount() + "")
+                .replace("$data$", itemStack.getMaterial().getDurability() + "")
+                .replace("$stack$", "itemStackStringMap" + slot)
+        );
+
+        buffer.addLine(((String) loreStringMap.get("create lore buffer"))
+                .replace("$id$", slot + "")
+        );
+        for (String lore_line : itemStack.getLoreAsList())
+            buffer.addLine(((String) loreStringMap.get("add item to lore buffer"))
+                    .replace("$id$", slot + "")
+                    .replace("$lore_line$", lore_line)
             );
+        buffer.addLine(((String) enchantmentStringMap.get("create ench buffer"))
+                .replace("$id$", slot + "")
+        );
+        for (Enchantment ench : itemStack.getEnchantments())
+            buffer.addLine(((String) enchantmentStringMap.get("add item to ench buffer"))
+                    .replace("$id$", slot + "")
+                    .replace("$ench$", ench.getBukkitName())
+                    .replace("$power$", ench.getPowerLavel() + "")
+            );
+        buffer.addLine(((String) itemStackStringMap.get("call formatter"))
+                .replace("$stack$", "itemStackStringMap" + slot)
+                .replace("$name$", itemStack.getName())
+                .replace("$loreBuffer$", "loreBuffer" + slot)
+                .replace("$enchantments$", "enchBuffer" + slot)
 
-            buffer.addLine(((String)lore.get("lore buffer"))
-                    .replace("$id$",slot+"")
-            );
-            for(String lore_line : itemStack1.getLoreAsList())
-                buffer.addLine(((String)lore.get("add item to lore buffer"))
-                        .replace("$id$",slot+"")
-                        .replace("$lore_line$",lore_line)
-                );
-            buffer.addLine(((String)lore.get("ench buffer"))
-                    .replace("$id$",slot+"")
-            );
-            for(Enchantment ench: itemStack1.getEnchantments())
-                buffer.addLine(((String)lore.get("add item to ench buffer"))
-                        .replace("$id$",slot+"")
-                        .replace("$ench$",ench.getBukkitName())
-                        .replace("$power$",ench.getPowerLavel()+"")
-                );
-            buffer.addLine(((String)itemStack.get("call formatter"))
-                    .replace("$stack$","itemStack"+slot)
-                    .replace("$name$",itemStack1.getName())
-                    .replace("$loreBuffer$","loreBuffer"+slot)
-                    .replace("$enchantments$","enchBuffer"+slot)
+        );
+    }
 
-            );
-        }
+    public static void main(String[] args) {
+        System.out.println("Testing:");
+        CodeBuffer buffer = CodeGenerator.getInstance().new CodeBuffer();
+        buffer.addLine("{");
+
+        ItemStack stack = new ItemStack(Material.BED);
+        getInstance().addItemstackToInv(stack, buffer, 1);
+        System.out.println(buffer.getFormattedOutput());
+        buffer.addLine("}");
     }
 }
